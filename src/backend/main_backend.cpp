6a5542231c9073a3a2fd01030255280cd0f54553
Benjamin Cabrera <benjamin.cabrera@uni-due.de>
2017-04-21 16:36:24 +0200

    Setup server for memory profiling

--- src/backend/main_backend.cpp
+++ src/backend/main_backend.cpp
@@ -1,4 +1,4 @@
-
+#include <thread>
 #include "../core/wikiDataCache.h"
 #include <boost/filesystem.hpp>
 
@@ -25,35 +25,41 @@ int main(int argc, char* argv[])
 	timer.start_timing_step("global","Reading in data files", &std::cout);
 
 	WikiDataCache data(data_path.string());
-	timer.start_timing_step("read_article_titles","Reading in 'article_titles'", &std::cout);
-	data.article_titles();
-	timer.stop_timing_step("read_article_titles",&std::cout);
-	timer.start_timing_step("read_category_titles","Reading in 'category_titles'", &std::cout);
-	data.category_titles();
-	timer.stop_timing_step("read_category_titles",&std::cout);
-	timer.start_timing_step("read_category_has_article","Reading in 'category_has_article'", &std::cout);
-	data.category_has_article();
-	timer.stop_timing_step("read_category_has_article",&std::cout);
-	timer.start_timing_step("read_article_dates","Reading in 'article_dates'", &std::cout);
-	data.article_dates();
-	timer.stop_timing_step("read_article_dates",&std::cout);
+	// timer.start_timing_step("read_article_titles","Reading in 'article_titles'", &std::cout);
+	// data.article_titles();
+	// timer.stop_timing_step("read_article_titles",&std::cout);
+	// timer.start_timing_step("read_category_titles","Reading in 'category_titles'", &std::cout);
+	// data.category_titles();
+	// timer.stop_timing_step("read_category_titles",&std::cout);
+	// timer.start_timing_step("read_category_has_article","Reading in 'category_has_article'", &std::cout);
+	// data.category_has_article();
+	// timer.stop_timing_step("read_category_has_article",&std::cout);
+	// timer.start_timing_step("read_article_dates","Reading in 'article_dates'", &std::cout);
+	// data.article_dates();
+	// timer.stop_timing_step("read_article_dates",&std::cout);
 	timer.start_timing_step("read_event_network","Reading in 'event_network'", &std::cout);
 	data.event_network();
 	timer.stop_timing_step("read_event_network",&std::cout);
-	timer.start_timing_step("read_event_indices","Reading in 'event_indices'", &std::cout);
-	data.event_indices();
-	timer.stop_timing_step("read_event_indices",&std::cout);
+	// timer.start_timing_step("read_event_indices","Reading in 'event_indices'", &std::cout);
+	// data.event_indices();
+	// timer.stop_timing_step("read_event_indices",&std::cout);
 
 	std::cout << "---------------------------------------------------" << std::endl;
 	std::cout << "IO Timings: " << std::endl;
 	timer.print_timings(std::cout);
 	std::cout << "---------------------------------------------------" << std::endl;
 
+
+	std::this_thread::sleep_for(std::chrono::seconds(10));
+
+
+
+
 	Shared::StepTimer timer_server;
 
 	timer_server.start_timing_step("server_running","Server running", &std::cout);
-	Server server(data);
-	server.run(argc,argv);
+	// Server server(data);
+	// server.run(argc,argv);
 	timer_server.stop_timing_step("server_running", &std::cout);
 
 	std::cout << "---------------------------------------------------" << std::endl;
