ce6a29d36665fd050433580f17a8324d1f34618f
Benjamin Cabrera <benny@bcabrera.de>
2017-04-22 21:06:02 +0200

    Tried to implemented partial s1 runner

--- src/parsers/main_s1_articlesAndCategories.cpp
+++ src/parsers/main_s1_articlesAndCategories.cpp
@@ -6,6 +6,7 @@
 #include <thread>
 #include <fstream>
 #include <functional>
+#include <mutex>
 
 // xerces
 #include <xercesc/sax2/SAX2XMLReader.hpp>
@@ -24,6 +25,7 @@
 // wiki xml dump lib
 #include "wikiArticleHandlers/articleDatesAndCategoriesHandler.h"
 #include "../../libs/wiki_xml_dump_xerces/src/parsers/parallelParser.hpp"
+#include "../../libs/wiki_xml_dump_xerces/src/parsers/singleCoreParser.hpp"
 #include "../../libs/wiki_xml_dump_xerces/src/handlers/wikiDumpHandlerProperties.hpp"
 #include "../../libs/wiki_xml_dump_xerces/src/handlers/basicTitleFilters.hpp"
 
@@ -105,51 +107,62 @@ int main(int argc, char* argv[])
 	// init xerces
 	xercesc::XMLPlatformUtils::Initialize();
 
+	// setup files and mutices
+	std::ofstream articles_file((outputFolder / WikiDataCache::ARTICLES_TITLES_FILE).string());	
+	std::ofstream categories_file((outputFolder / WikiDataCache::CATEGORIES_TITLES_FILE).string());	
+	std::ofstream redirects_file((outputFolder / WikiDataCache::REDIRECTS_FILE).string());	
+	std::ofstream article_dates_file((outputFolder / WikiDataCache::ARTICLE_DATES_FILE).string());	
+
+	std::mutex article_titles_mutex, category_titles_mutex, redirect_mutex;
+
+
 	WikiXmlDumpXerces::WikiDumpHandlerProperties parser_properties;
 	parser_properties.TitleFilter = WikiXmlDumpXerces::only_articles_and_categories();
 	parser_properties.ProgressCallback = std::bind(printProgress, pageCounts, std::placeholders::_2, std::placeholders::_1, std::placeholders::_3);
 	parser_properties.ProgressReportInterval = 5000;
 
 	// WikiXmlDumpXerces::ParallelParser<ArticleDatesAndCategoriesHandler> parser([](){ return ArticleDatesAndCategoriesHandler(&std::cout); }, parser_properties);
-	WikiXmlDumpXerces::ParallelParser<ArticleDatesAndCategoriesHandler> parser([](){ return ArticleDatesAndCategoriesHandler(); }, parser_properties);
+	WikiXmlDumpXerces::ParallelParser<ArticleDatesAndCategoriesHandler> parser([&articles_file, &categories_file, &redirects_file, &article_dates_file, &article_titles_mutex, &category_titles_mutex, &redirect_mutex](){ return ArticleDatesAndCategoriesHandler(articles_file,article_dates_file,categories_file,redirects_file,article_titles_mutex,category_titles_mutex,redirect_mutex); }, parser_properties);
+	// ArticleDatesAndCategoriesHandler handler(articles_file,article_dates_file,categories_file,redirects_file,article_titles_mutex,category_titles_mutex,redirect_mutex);
+	// WikiXmlDumpXerces::SingleCoreParser parser(handler, parser_properties);
 	parser.Run(paths.begin(), paths.end());
 
 	// terminate xerces
 	xercesc::XMLPlatformUtils::Terminate();
 
-	std::map<std::string,std::vector<Date>> articlesWithDates;
-	std::vector<std::string> category_titles;
-	std::map<std::string,std::string> redirects;
-	std::size_t n_errors = 0;
+	// std::map<std::string,std::vector<Date>> articlesWithDates;
+	// std::vector<std::string> category_titles;
+	// std::map<std::string,std::string> redirects;
+	// std::size_t n_errors = 0;
 
-	for (auto& hand : parser.PageHandlers())
-	{
-		articlesWithDates.insert(hand.articles.begin(), hand.articles.end());
-		category_titles.insert(category_titles.end(), hand.categories.begin(), hand.categories.end());
-		redirects.insert(hand.redirects.begin(), hand.redirects.end());
-		n_errors += hand.n_errors;
-	}
+	// for (auto& hand : parser.PageHandlers())
+	// {
+	// articlesWithDates.insert(hand.articles.begin(), hand.articles.end());
+	// category_titles.insert(category_titles.end(), hand.categories.begin(), hand.categories.end());
+	// redirects.insert(hand.redirects.begin(), hand.redirects.end());
+	// n_errors += hand.n_errors;
+	// }
 
 
 	timer.stop_timing_step("parsing");
 
 
-	timer.start_timing_step("sorting", "Sorting categories vector", &std::cout);
-	std::sort(category_titles.begin(), category_titles.end());
-	timer.stop_timing_step("sorting");
+	// timer.start_timing_step("sorting", "Sorting categories vector", &std::cout);
+	// std::sort(category_titles.begin(), category_titles.end());
+	// timer.stop_timing_step("sorting");
 
-	timer.start_timing_step("output", "Writing output files", &std::cout);
+	// timer.start_timing_step("output", "Writing output files", &std::cout);
 
-	WikiDataCache wiki_data_cache(outputFolder.string());
-	wiki_data_cache.write_article_titles(articlesWithDates);
-	wiki_data_cache.write_category_titles(category_titles);
-	wiki_data_cache.write_article_dates(articlesWithDates);
-	wiki_data_cache.write_redirects(redirects);
+	// WikiDataCache wiki_data_cache(outputFolder.string());
+	// wiki_data_cache.write_article_titles(articlesWithDates);
+	// wiki_data_cache.write_category_titles(category_titles);
+	// wiki_data_cache.write_article_dates(articlesWithDates);
+	// wiki_data_cache.write_redirects(redirects);
 
-	timer.stop_timing_step("output");
+	// timer.stop_timing_step("output");
 
 	timer.stop_timing_step("global");
-	
+
 	// short feedback to user
 	std::size_t totalArticleNumber = 0;
 	for (auto path : paths) {
@@ -160,11 +173,11 @@ int main(int argc, char* argv[])
 	std::cout << "-----------------------------------------------------------------------" << std::endl;
 	std::cout << "Status: " << std::endl;
 	std::cout << std::left << std::setw(40) << "Total number of pages: " << totalArticleNumber << std::endl;
-	std::cout << std::left << std::setw(40) << "Articles with extractable dates: " << articlesWithDates.size() << std::endl;
-	std::cout << std::left << std::setw(40) << "Number of date extraction errors: " << n_errors << std::endl;
-	std::cout << std::left << std::setw(40) << "Categories extracted: " << category_titles.size() << std::endl;
-	std::cout << std::left << std::setw(40) << "Redirects extracted: " << redirects.size() << std::endl;
-	
+	// std::cout << std::left << std::setw(40) << "Articles with extractable dates: " << articlesWithDates.size() << std::endl;
+	// std::cout << std::left << std::setw(40) << "Number of date extraction errors: " << n_errors << std::endl;
+	// std::cout << std::left << std::setw(40) << "Categories extracted: " << category_titles.size() << std::endl;
+	// std::cout << std::left << std::setw(40) << "Redirects extracted: " << redirects.size() << std::endl;
+
 	// output timings
 	std::cout << "-----------------------------------------------------------------------" << std::endl;
 	std::cout << "Timings: " << std::endl << std::endl;
--- src/parsers/wikiArticleHandlers/articleDatesAndCategoriesHandler.h
+++ src/parsers/wikiArticleHandlers/articleDatesAndCategoriesHandler.h
@@ -3,19 +3,30 @@
 #include <vector>
 #include <set>
 #include <map>
+#include <mutex>
 
 #include "../../../libs/wiki_xml_dump_xerces/src/handlers/abstractWikiPageHandler.hpp"
 #include "../../core/date.h"
 
 class ArticleDatesAndCategoriesHandler : public WikiXmlDumpXerces::AbstractWikiPageHandler {
 	public:
-		ArticleDatesAndCategoriesHandler(std::ostream* report_ostr = nullptr);
+		static constexpr std::size_t WRITE_INTERVAL = 10000;
+
+		ArticleDatesAndCategoriesHandler(
+			std::ostream& article_titles_file,
+			std::ostream& article_dates_file,
+			std::ostream& category_titles_file,
+			std::ostream& redirect_file,
+			std::mutex& _article_titles_mutex,
+			std::mutex& _category_titles_mutex,
+			std::mutex& _redirect_mutex,
+			std::ostream* report_ostr = nullptr
+		);
+
+		~ArticleDatesAndCategoriesHandler();
 
 		void HandleArticle(const WikiXmlDumpXerces::WikiPageData&);
 
-		std::map<std::string, std::vector<Date>> articles;
-		std::vector<std::string> categories;
-		std::map<std::string, std::string> redirects;
 
 		bool ExtractOnlyArticlesWithDates;
 
@@ -23,4 +34,21 @@ class ArticleDatesAndCategoriesHandler : public WikiXmlDumpXerces::AbstractWikiP
 
 	private:
 		std::ostream* _report_ostr;
+
+		void write_article_titles();
+		void write_category_titles();
+		void write_redirects();
+
+		std::map<std::string, std::vector<Date>> _articles;
+		std::vector<std::string> _categories;
+		std::map<std::string, std::string> _redirects;
+
+		std::ostream& _article_titles_file;
+		std::ostream& _article_dates_file;
+		std::ostream& _category_titles_file;
+		std::ostream& _redirect_file;
+
+		std::mutex& _article_titles_mutex;
+		std::mutex& _category_titles_mutex;
+		std::mutex& _redirect_mutex;
 };
