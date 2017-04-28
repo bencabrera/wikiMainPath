4b05551fb12c8e1ecbf4714d0aa64068f0512bf8
Benjamin Cabrera <benjamin.cabrera@uni-due.de>
2017-04-28 17:23:52 +0200

    Implemented manual derivatives

--- src/backend/graphDrawing/forceDirectedGraphDrawing.cpp
+++ src/backend/graphDrawing/forceDirectedGraphDrawing.cpp
@@ -1,6 +1,5 @@
 #include "forceDirectedGraphDrawing.h"
 #include <random>
-#include <unsupported/Eigen/NonLinearOptimization>
 #include <iostream>
 
 std::string flag_to_string(int flag)
@@ -23,119 +22,133 @@ std::string flag_to_string(int flag)
 	return m.at(flag);
 }
 
-struct Functor
+Functor::Functor(int inputs, int values) : m_inputs(inputs), m_values(values) {}
+int Functor::inputs() const { return m_inputs; }
+int Functor::values() const { return m_values; }
+
+
+ForceDirectedFunctor::ForceDirectedFunctor(const ArticleGraph& event_network, const std::vector<double>& x_positions)
+	:Functor(x_positions.size(), x_positions.size() + 2 * x_positions.size() * (x_positions.size() - 1)),
+	_event_network(event_network),
+	_x_positions(x_positions),
+	_n(x_positions.size())
+{}
+
+double ForceDirectedFunctor::r(double s) const
 {
-	using InputType = Eigen::VectorXd;
-	using ValueType = Eigen::VectorXd;
-	using Matrix = Eigen::MatrixXd;
-	using Scalar = double;
-	using JacobianType = Matrix;
+	return std::pow(s, -_alpha);
+}
 
-	enum {
-		InputsAtCompileTime = Eigen::Dynamic,
-		ValuesAtCompileTime = Eigen::Dynamic
-	};
-
-	const int m_inputs, m_values;
-
-	Functor(int inputs, int values) : m_inputs(inputs), m_values(values) {}
-
-	int inputs() const { return m_inputs; }
-	int values() const { return m_values; }
-};
-
-class ForceDirectedFunctor : public Functor
+double ForceDirectedFunctor::ds_r(double s) const
 {
-	private:
-		static constexpr double _zeta = 1;
+	return std::pow(s, -_alpha-1);
+}
 
-		static constexpr double _alpha = 1;
-		static constexpr double _beta = 1;
-		static constexpr double _gamma = 1;
+double ForceDirectedFunctor::norm(double x1, double x2, double y1, double y2) const
+{
+	return std::sqrt(std::pow(x1-x2,2) + std::pow(y1-y2,2));
+}
 
-		
-		const ArticleGraph& _event_network;
-		const std::vector<double>& _x_positions;
-		const std::size_t _n;
+double ForceDirectedFunctor::dy1_norm(double x1, double x2, double y1, double y2) const
+{
+	return -(y1 - y2) / norm(x1,x2,y1,y2);
+}
 
-	public:
-		ForceDirectedFunctor(const ArticleGraph& event_network, const std::vector<double>& x_positions)
-			:Functor(x_positions.size(), x_positions.size() + 2 * x_positions.size() * (x_positions.size() - 1)),
-			_event_network(event_network),
-			_x_positions(x_positions),
-			_n(x_positions.size())
-	{}
+double ForceDirectedFunctor::dy2_norm(double x1, double x2, double y1, double y2) const
+{
+	return (y1 - y2) / norm(x1,x2,y1,y2);
+}
 
-		double r(double s) const
-		{
-			return std::pow(s, -_alpha);
-		}
+int ForceDirectedFunctor::operator()(const InputType& y, ValueType& fvec) const
+{
+	fvec.setZero();
 
-		double ds_r(double s) const
-		{
-			return std::pow(s, -_alpha-1);
-		}
+	// std::cout << "y: " << y << std::endl;
+	for(std::size_t i = 0; i < _n; i++)
+	{
+		// fvec(i) = r(y(i)) + r(1-y(i));
+		fvec(i) = _alpha * (r(y(i)) + r(1-y(i))+ 25 * std::pow(y(i)-0.5,2));
+		// fvec(i) = (y(i) - 0.5) * (y(i) - 0.5);
+	}
 
-		double norm(double x1, double x2, double y1, double y2) const
-		{
-			return std::sqrt(std::pow(x1-x2,2) + std::pow(y1-y2,2));
-		}
-
-		int operator()(const InputType& y, ValueType& fvec) const
-		{
-			fvec.setZero();
-
-			// std::cout << "y: " << y << std::endl;
-			for(std::size_t i = 0; i < _n; i++)
+	std::size_t cur_pos = _n;
+	for(std::size_t i = 0; i < _n; i++)
+		for(std::size_t j = 0; j < _n; j++)
+			if(i != j)
 			{
-				// fvec(i) = r(y(i)) + r(1-y(i));
-				fvec(i) = _alpha * (r(y(i)) + r(1-y(i))+ 25 * std::pow(y(i)-0.5,2));
-				// fvec(i) = (y(i) - 0.5) * (y(i) - 0.5);
+				if(boost::edge(i, j, _event_network).second)
+					fvec(cur_pos) = _beta * std::pow(y(i) - y(j), 2.0);
+
+				cur_pos++;
 			}
 
-			std::size_t cur_pos = _n;
-			for(std::size_t i = 0; i < _n; i++)
-				for(std::size_t j = 0; j < _n; j++)
-					if(i != j)
-					{
-						if(boost::edge(i, j, _event_network).second)
-							fvec(cur_pos) = _beta * std::pow(y(i) - y(j), 2.0);
-
-						cur_pos++;
-					}
-
-			for(std::size_t i = 0; i < _n; i++)
-				for(std::size_t j = 0; j < _n; j++)
-					if(i != j)
-					{
-						fvec(cur_pos) = _gamma * r(norm(_x_positions[i],_x_positions[j],y(i),y(j)));
-
-						cur_pos++;
-					}
-			// std::cout << "fvec: " << std::endl << fvec << std::endl;
-
-			return 0;
-		}
-
-		int df(const InputType& y, Matrix& fjac) const
-		{
-			fjac.setZero();
-
-			// std::cout << "y: " << y << std::endl;
-			for(std::size_t i = 0; i < _n; i++)
+	for(std::size_t i = 0; i < _n; i++)
+		for(std::size_t j = 0; j < _n; j++)
+			if(i != j)
 			{
-				fjac(i,i) = - ds_r(y(i)) + ds_r(1-y(i));
-			}
-			// std::cout << "fjac: " << fjac << std::endl;
+				fvec(cur_pos) = _gamma * r(norm(_x_positions[i],_x_positions[j],y(i),y(j)));
 
-			return 0;
-		}
-};
+				cur_pos++;
+			}
+	// std::cout << "fvec: " << std::endl << fvec << std::endl;
+
+	return 0;
+}
+
+int ForceDirectedFunctor::df(const InputType& y, Matrix& fjac) const
+{
+	fjac.setZero();
+
+	// std::cout << "y: " << y << std::endl;
+	for(std::size_t i = 0; i < _n; i++)
+	{
+		fjac(i,i) = - ds_r(y(i)) + ds_r(1-y(i)) + 25 * 2 * (y(i) - 0.5);
+	}
+
+	std::size_t cur_pos = _n;
+	for(std::size_t i = 0; i < _n; i++)
+		for(std::size_t j = 0; j < _n; j++)
+			if(i != j)
+			{
+				if(boost::edge(i, j, _event_network).second)
+				{
+					fjac(cur_pos, i) = _beta * 2 * (y(i) - y(j));
+					fjac(cur_pos, j) = _beta * -2 * (y(i) - y(j));
+				}
+
+				cur_pos++;
+			}
+
+	// fvec(cur_pos) = _gamma * r(norm(_x_positions[i],_x_positions[j],y(i),y(j)));
+	for(std::size_t i = 0; i < _n; i++)
+		for(std::size_t j = 0; j < _n; j++)
+			if(i != j)
+			{
+				fjac(cur_pos, i) = 
+					_gamma 
+					* ds_r(norm(_x_positions[i],_x_positions[j],y(i),y(j))) 
+					* dy1_norm(_x_positions[i],_x_positions[j],y(i),y(j));
+
+				fjac(cur_pos, j) = 
+					_gamma 
+					* ds_r(norm(_x_positions[i],_x_positions[j],y(i),y(j))) 
+					* dy2_norm(_x_positions[i],_x_positions[j],y(i),y(j));
+
+				cur_pos++;
+			}
+
+	// std::cout << "fjac: " << fjac << std::endl;
+
+	return 0;
+}
 
 std::vector<double> force_directed_graph_drawing(const ArticleGraph& event_network, const std::vector<double>& x_positions)
 {
 	ForceDirectedFunctor f(event_network, x_positions);
 	Eigen::NumericalDiff<ForceDirectedFunctor> f_numerical_diff(f);
+
+
+
 	Eigen::LevenbergMarquardt<Eigen::NumericalDiff<ForceDirectedFunctor>, double> lm_solver(f_numerical_diff);
 	// Eigen::LevenbergMarquardt<ForceDirectedFunctor, double> lm_solver(f);
 
@@ -146,13 +159,6 @@ std::vector<double> force_directed_graph_drawing(const ArticleGraph& event_netwo
 	std::uniform_real_distribution<double> unif_0_1;
 	for(int i = 0; i < y.size(); i++)
 		y(i) = unif_0_1(rand_gen);
-
-	// y.setConstant(0.4);
-	// y.setRandom();
-
-	ForceDirectedFunctor::ValueType residual(f.values());
-	f(y, residual);
-	// f(y);
 
 	int flag = lm_solver.minimize(y);
 	std::cout << "FLAG: " << flag_to_string(flag) << std::endl;
--- src/backend/tests/forceDirectedGraphDrawingTests.cpp
+++ src/backend/tests/forceDirectedGraphDrawingTests.cpp
@@ -4,6 +4,7 @@
 #include <streambuf>
 #include <string>
 #include <iostream>
+#include <random>
 
 #include "../graphDrawing/forceDirectedGraphDrawing.h"
 
@@ -34,6 +35,44 @@ BOOST_AUTO_TEST_CASE(test2)
 
 	std::cout << "Positions: " << std::endl;
 	std::cout << y[0] << ", " << y[1] << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(test3)
+{
+	ArticleGraph g;
+	auto v1 = boost::add_vertex(g);
+	auto v2 = boost::add_vertex(g);
+	auto v3 = boost::add_vertex(g);
+	auto v4 = boost::add_vertex(g);
+	boost::add_edge(v1, v2, g);
+	boost::add_edge(v2, v3, g);
+	boost::add_edge(v1, v4, g);
+	boost::add_edge(v2, v4, g);
+	boost::add_edge(v4, v3, g);
+
+	std::vector<double> x_positions = { 0.2, 0.4, 0.6, 0.8 };
+
+	ForceDirectedFunctor f(g, x_positions);
+	Eigen::NumericalDiff<ForceDirectedFunctor> f_numerical_diff(f);
+
+	ForceDirectedFunctor::InputType y(f.inputs());
+
+	std::mt19937 rand_gen(42);
+	std::uniform_real_distribution<double> unif_0_1;
+	for(int i = 0; i < y.size(); i++)
+		y(i) = unif_0_1(rand_gen);
+	
+	ForceDirectedFunctor::JacobianType mat1(f.values(), f.inputs()), mat2(f.values(), f.inputs());
+	f.df(y,mat1);
+	f_numerical_diff.df(y,mat2);
+
+	ForceDirectedFunctor::JacobianType m = mat1 - mat2;
+	for(int i = 0; i < m.rows(); i++)
+		for(int j = 0; j < m.cols(); j++)
+			if(m(i,j) < 1e-7)
+				m(i,j) = 0.0;
+
+	BOOST_TEST(m.norm() == 0.0, boost::test_tools::tolerance(1e-10));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
