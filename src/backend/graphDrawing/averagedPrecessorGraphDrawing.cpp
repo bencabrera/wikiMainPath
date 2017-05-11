272d84862fe52124f2a9864fe48980ba8292100c
Benjamin Cabrera <benjamin.cabrera@uni-due.de>
2017-05-11 16:59:31 +0200

    Added ability to use topological sort on averaged positioning

--- src/backend/graphDrawing/averagedPrecessorGraphDrawing.cpp
+++ src/backend/graphDrawing/averagedPrecessorGraphDrawing.cpp
@@ -1,5 +1,8 @@
 #include "randomGraphDrawing.h"
 #include <random>
+#include <boost/graph/topological_sort.hpp>
+#include <boost/range/adaptor/reversed.hpp>
+
 
 namespace WikiMainPath {
 	namespace GraphDrawing {
@@ -19,18 +22,22 @@ namespace WikiMainPath {
 					positions[i] = cur_pos;
 				}
 
+			// std::vector<ArticleGraph::vertex_descriptor> topological_order;
+			// boost::topological_sort(event_network, std::back_inserter(topological_order));
 
-			for(std::size_t i = 0; i < boost::num_vertices(event_network); i++)
-				if(boost::in_degree(i, event_network) == 0)
+			// for(std::size_t v = 0; v < boost::num_vertices(event_network); v++)
+			for(auto v : boost::make_iterator_range(boost::vertices(event_network))) // output from boost::topological_sort is in reverse top. order => reverse again
+			// for(auto v : boost::adaptors::reverse(topological_order)) // output from boost::topological_sort is in reverse top. order => reverse again
+				if(boost::in_degree(v, event_network) == 0)
 					continue;
 				else {
 					// compute average of incoming neighbours
 					double avg = 0.0;
-					for (auto e : boost::make_iterator_range(boost::in_edges(i,event_network)))
+					for (auto e : boost::make_iterator_range(boost::in_edges(v,event_network)))
 						avg += positions[boost::source(e,event_network)];
-					avg = avg / boost::in_degree(i,event_network);
+					avg = avg / boost::in_degree(v,event_network);
 
-					positions[i] = avg;
+					positions[v] = avg;
 				}
 
 			return positions;
